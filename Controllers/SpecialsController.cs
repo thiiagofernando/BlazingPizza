4e603146b6537e78149f1d9c22ac6912916c9f35
Thiago Fernando <thiago.fernando@msn.com>
2024-03-03 17:25:38 -0400

    inicial

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,24 @@
+using BlazingPizza.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpecialsController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+        public SpecialsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("lista")]
+        public async Task<IActionResult> GetSpecials()
+        {
+            var lista = (await _db.Specials.ToListAsync()).OrderByDescending(x => x.BasePrice).ToList();
+            return Ok(lista);
+        }
+    }
+}
